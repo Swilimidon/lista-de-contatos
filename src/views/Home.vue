3a6c5680383267608909323ac25dff3b7b697eaa
Kainã Barros <kainamartinho@hotmail.com>
2020-04-17 17:32:54 +0000

    import

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,17 +15,43 @@
             </a>
           </template>
         </b-autocomplete>
-      </b-field>
+    </b-field>
 
-      <the-card v-show="selected"
-        v-bind="selected">
-      </the-card>
-      <hr>
+    <the-card v-show="selected"
+      v-bind="selected">
+    </the-card>
+    <hr>
+    <b-field grouped position="is-right">
+      <div class="buttons">
+        <b-tooltip label="Tooltip right"
+            position="is-top"
+            :active="active">
+          <b-button type="is-info" size="is-small" class="clipboard" :data-clipboard-text="this.export" @click="success">Export</b-button>
+        </b-tooltip>
+
+        <b-button type="is-info" size="is-small" @click="isCardModalActive = true">Import</b-button>
+      </div>
+      </b-field>
     <b-table :data="lista" :columns="columns"></b-table>
+
+    <b-modal :active.sync="isCardModalActive" :widht="480" scroll="keep">
+      <div class="card">
+        <div class="card-content">
+          <div class="content">
+            <b-input type="textarea" id="import"></b-input>
+            <b-button @click="save" type="is-info">Importar</b-button>
+          </div>
+        </div>
+      </div>
+    </b-modal>
   </div>
 </template>
 <script>
 import TheCard from '@/components/TheCard.vue'
+import ClipboardJS from 'clipboard/dist/clipboard'
+
+/* eslint-disable no-new */
+new ClipboardJS('.clipboard')
 
 export default {
   name: 'Home',
@@ -54,7 +80,9 @@ export default {
         }
       ],
       selected: '',
-      name: ''
+      name: '',
+      isCardModalActive: false,
+      active: false
     }
   },
   computed: {
@@ -76,12 +104,25 @@ export default {
       } else {
         return []
       }
+    },
+    export () {
+      return JSON.stringify(this.lista)
     }
   },
   methods: {
     goTO () {
       this.$router.push('add-contact')
+    },
+    success () {
+      this.$buefy.toast.open({
+        message: 'Copiado para área de transferência!'
+      })
     }
+    /* importar () {
+      console.log(document.getElementById('#import').getValue())
+
+      window.localStorage.setItem('lista', JSON.stringify(this.lista))
+    } */
   }
 }
 </script>
